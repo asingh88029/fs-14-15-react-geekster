cbd25370dfc5c554227073e063e39a9c62be36a8
asingh88029 <asingh88029@gmail.com>
2024-03-18 23:12:08 +0530

    QUIZ APP Almost Done

--- quiz/src/Question.js
+++ quiz/src/Question.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React from 'react';
+
+const PrevBTN = ({onClick})=>{
+  return (
+    <div>
+      <button 
+        onClick={()=>{
+          onClick()
+        }}
+      >
+        Prev
+      </button>
+    </div>
+  )
+}
+
+const NextBTN = ({onClick})=>{
+  return (
+    <div>
+      <button
+        onClick={()=>{
+          onClick()
+        }}
+      >
+        Next
+      </button>
+    </div>
+  )
+}
+
+const SubmitBTN = ({onClick})=>{
+
+  return (
+    <div>
+      <button
+        onClick={()=>{
+          onClick()
+        }}
+      >Submit QUIZ</button>
+    </div>
+  )
+
+}
 
 const Question = ({questionData, setQuestionData, currentQuestionIndex, setCurrentQuestionIndex}) => {
   return (
     <div>
         <h4>Question{questionData[currentQuestionIndex].question_no} : {questionData[currentQuestionIndex].question}</h4>
-        {questionData[currentQuestionIndex]?.options?.map(option=><button>{option}</button>)}
+        {questionData[currentQuestionIndex]?.options?.map(option=><div><button>{option}</button></div>)}
+        {currentQuestionIndex!=0 && 
+        <PrevBTN onClick={()=>{
+          setCurrentQuestionIndex(prevValue=>prevValue-1)
+        }}/>}
+        {currentQuestionIndex==questionData.length-1 ? <SubmitBTN onClick={()=>{}}/> : <NextBTN onClick={()=>{setCurrentQuestionIndex(prevValue=>prevValue+1)}}/> }
     </div>
   )
 }
